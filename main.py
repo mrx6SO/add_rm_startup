fbb7add0b6de725c4a983741b4bc97d949b19d01
mrx6SO <53888062+mrx6SO@users.noreply.github.com>
2020-01-06 22:51:52 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from cls_strt import *
+
+def main():
+
+    ch = raw_input("Embedded the file or remove:[E|R]")
+
+    if(ch == 'E'):
+        
+        a = Start()
+        a.add_to_startup()
+    
+    elif(ch == 'R'):
+        
+        rm_strt()
+        
+    else:
+
+        pass
+    
+if __name__ == "__main__":
+    
+    main()
+
+    
+
+    
